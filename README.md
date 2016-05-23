ce916b6d88b08189754601cfe6315bb1721bd9cb
valmassoi <rvalmassoi@gmail.com>
2016-05-22 23:01:01 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# yelp-on
+A full stack JavaScript app using `MERN` Stack `MongoDB` + `Express.js` + `React` + `Node.js` + `flux` + `Web Sockets`
+
+## Live Demo
+Hosted at https://bowlish.herokuapp.com
+
+## Built for FreeCodeCamp
+https://www.freecodecamp.com/challenges/chart-the-stock-market
+
+## Features
+User Story: view a graph displaying the recent trend lines for each added stock.  
+User Story: add new stocks by their symbol name.  
+User Story: remove stocks.  
+User Story: see changes in real-time when any other user adds or removes a stock.
+
+## TODO
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var debug = process.env.NODE_ENV !== "production"
+var webpack = require('webpack')
+var path = require('path')
+
+module.exports = {
+  context: path.join(__dirname, "public"),
+  devtool: debug ? "inline-sourcemap" : null,
+  entry: "./js/routes.js",
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+          plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+        }
+      },
+      {
+        test: /\.sass$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: ["style", "css", "sass"]
+      }
+    ]
+  },
+  sassLoader: {
+    includePaths: [path.resolve(__dirname, "./public/styles")]
+  },
+  output: {
+    path: __dirname + "/public/",
+    filename: "routes.min.js"
+  },
+  plugins: debug ? [] : [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+  ],
+}
